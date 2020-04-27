c5a37f612fa68662a69f66df61f2a0d431f9e913
Ridup <mu_ran@yahoo.com>
2020-04-27 20:32:00 +0800

    feat(kafka

---
+++ kafka-chat-server/src/test/java/com/shubh/javakafka/KafkaJavaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shubh.javakafka;
+
+import com.ridup.chat.KafkaJavaApp;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = KafkaJavaApp.class)
+class KafkaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
